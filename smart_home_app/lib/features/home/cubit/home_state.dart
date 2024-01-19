022aa55d59986b7ce7c0c936d2a132e25e6a53e8
ahmed allawy <m_ahmed20156947@yahoo.com>
2024-01-19 15:23:56 +0200

    smart home app with mqtt protocol

--- smart_home_app/lib/features/home/cubit/home_state.dart
+++ smart_home_app/lib/features/home/cubit/home_state.dart
@@ -3,7 +3,5 @@ abstract class HOMESTATE {}
 class InitialState extends HOMESTATE {}
 
 class activeDevicesState extends HOMESTATE {}
-class ReceivedDataState extends HOMESTATE {}
-// class SubscribeToTopicState extends HOMESTATE {}
 
-// class ReceivedDataState extends HOMESTATE {}
+class ReceivedDataState extends HOMESTATE {}
