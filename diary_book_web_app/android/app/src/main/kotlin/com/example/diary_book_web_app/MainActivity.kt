b1e43206533e9cedef832056cb2ee51482a684da
ahmed allawy <m_ahmed20156947@yahoo.com>
2024-01-29 16:37:22 +0200

    first commit

---
+++ diary_book_web_app/android/app/src/main/kotlin/com/example/diary_book_web_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diary_book_web_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ diary_book_web_app/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Diary Book',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Colors.green,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        toolbarHeight: 100,
+        elevation: 4,
+      ),
+    );
+  }
+}
